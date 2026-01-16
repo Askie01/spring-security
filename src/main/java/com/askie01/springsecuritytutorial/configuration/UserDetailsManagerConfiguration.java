84e3f55332fc0941d3c16af06cfc8b7bdce67f79
Askie01 <adam.paw01@gmail.com>
2026-01-16 23:49:54 +0100

    Update `UserDetailsManagerConfiguration`
    * Updated mentioned configuration class with private method to create users.
    * This should clean up the user creation process.

--- src/main/java/com/askie01/springsecuritytutorial/configuration/UserDetailsManagerConfiguration.java
+++ src/main/java/com/askie01/springsecuritytutorial/configuration/UserDetailsManagerConfiguration.java
@@ -1,23 +1,36 @@
 package com.askie01.springsecuritytutorial.configuration;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 
 import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class UserDetailsManagerConfiguration {
+
+    private final PasswordEncoder passwordEncoder;
 
     @Bean
     public UserDetailsManager userDetailsManager() {
         final List<UserDetails> users = List.of(
-                User.builder().username("user").password("$2a$12$qPLrxVoWJ1SycwdnCR9wju3LoRH/HDkTNBTYRY8va59giqIecmjd2").build(),
-                User.builder().username("root").password("$2a$12$D7QkpqFit.Dhvq4wUm3O0.YR6Xtv6huFjph7DGQROrhipJN9gt3qi").build()
+                createUser("user", "user"),
+                createUser("admin", "admin")
         );
         return new InMemoryUserDetailsManager(users);
+    }
+
+    private UserDetails createUser(String name, String password) {
+        final String encodedPassword = passwordEncoder.encode(password);
+        return User.builder().
+                username(name)
+                .password(encodedPassword)
+                .build();
     }
 }
