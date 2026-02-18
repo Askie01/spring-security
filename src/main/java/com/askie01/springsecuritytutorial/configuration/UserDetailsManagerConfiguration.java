07ce916711b633f0f28e5eafad1195ce7e4fbc52
Adam Pawlowski <adam.paw01@gmail.com>
2026-02-18 13:18:51 +0100

    Combine `InMemoryUserDetailsManagerConfiguration` & `JdbcUserDetailsManagerConfiguration` into a single `UserDetailsManagerConfiguration`
    * Merged two `UserDetailsManager` configuration classes into a single one & enabled `JdbcUserDetailsManager` bean as a default `UserDetailsManager` in our application.

--- src/main/java/com/askie01/springsecuritytutorial/configuration/UserDetailsManagerConfiguration.java
+++ src/main/java/com/askie01/springsecuritytutorial/configuration/UserDetailsManagerConfiguration.java
@@ -1,23 +1,42 @@
 package com.askie01.springsecuritytutorial.configuration;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 
+import javax.sql.DataSource;
 import java.util.List;
 
-//@Configuration
+@Configuration
 @RequiredArgsConstructor
-public class InMemoryUserDetailsManagerConfiguration {
+public class UserDetailsManagerConfiguration {
 
     private final PasswordEncoder passwordEncoder;
 
     @Bean
-    public UserDetailsManager userDetailsManager() {
+    @Primary
+    @ConditionalOnProperty(
+            name = "component.manager.user-details",
+            havingValue = "jdbc"
+    )
+    public UserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    @ConditionalOnProperty(
+            name = "component.manager.user-details",
+            havingValue = "in-memory"
+    )
+    public UserDetailsManager inMemoryUserDetailsManager() {
         final List<UserDetails> users = List.of(
                 createUser("user", "user@123400000"),
                 createUser("admin", "admin@123400000")
