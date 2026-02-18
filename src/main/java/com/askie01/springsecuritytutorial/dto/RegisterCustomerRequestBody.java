d0e12e260ddd3a0641cb5456d69717617f388c49
Adam Pawlowski <adam.paw01@gmail.com>
2026-02-18 13:16:58 +0100

    Rename `email` to `username` in `RegisterCustomerRequestBody` DTO class
    * Renamed one field in the mentioned class to better match with username + password login/registration style.

--- src/main/java/com/askie01/springsecuritytutorial/dto/RegisterCustomerRequestBody.java
+++ src/main/java/com/askie01/springsecuritytutorial/dto/RegisterCustomerRequestBody.java
@@ -10,6 +10,6 @@ import lombok.*;
 @ToString
 @EqualsAndHashCode
 public class RegisterCustomerRequestBody {
-    private String email;
+    private String username;
     private String password;
 }
