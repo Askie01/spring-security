056222301384c4f0b3505bbc0c1ecb9812b81306
Adam Pawlowski <adam.paw01@gmail.com>
2026-02-18 13:21:07 +0100

    Rename `SecurityConfiguration` to `SecurityFilterChainConfiguration`
    * Renamed the mentioned class to better explain what "security" configuration it's actually doing.

--- src/main/java/com/askie01/springsecuritytutorial/configuration/SecurityFilterChainConfiguration.java
+++ src/main/java/com/askie01/springsecuritytutorial/configuration/SecurityFilterChainConfiguration.java
@@ -1,18 +1,30 @@
 package com.askie01.springsecuritytutorial.configuration;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfiguration {
+@RequiredArgsConstructor
+public class SecurityFilterChainConfiguration {
+
+    private final AuthenticationEntryPoint authenticationEntryPoint;
 
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
-        return http.authorizeHttpRequests(requestConfiguration -> requestConfiguration
+        return http
+                .sessionManagement(sessionConfiguration -> sessionConfiguration
+                        .invalidSessionUrl("/error/invalid-session")
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(true)
+                        .expiredUrl("/error/session-expired")
+                )
+                .authorizeHttpRequests(requestConfiguration -> requestConfiguration
                         .requestMatchers("/accounts").authenticated()
                         .requestMatchers("/cards").authenticated()
                         .requestMatchers("/loans").authenticated()
@@ -21,9 +33,12 @@ public class SecurityConfiguration {
                         .requestMatchers("/contacts").permitAll()
                         .requestMatchers("/error").permitAll()
                         .requestMatchers("/customers").permitAll()
+                        .requestMatchers("/error/invalid-session").permitAll()
+                        .requestMatchers("/error/session-expired").permitAll()
                 )
                 .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults())
+                .httpBasic(httpBasic -> httpBasic
+                        .authenticationEntryPoint(authenticationEntryPoint))
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
