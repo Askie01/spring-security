023097c08b47bae0dd0d1702a2d80ec4d8fda9c8
Adam Pawlowski <adam.paw01@gmail.com>
2026-02-18 13:15:11 +0100

    Update `loadUserByUsername` in  `CustomerUserDetailsService`
    * Updated method to load users by username, instead of email in `CustomerUserDetailsService` method.

--- src/main/java/com/askie01/springsecuritytutorial/service/CustomerUserDetailsService.java
+++ src/main/java/com/askie01/springsecuritytutorial/service/CustomerUserDetailsService.java
@@ -3,32 +3,31 @@ package com.askie01.springsecuritytutorial.service;
 import com.askie01.springsecuritytutorial.model.Customer;
 import com.askie01.springsecuritytutorial.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
-@Service
 @RequiredArgsConstructor
 public class CustomerUserDetailsService implements UserDetailsService {
 
     private final CustomerRepository repository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(@NonNull String username) throws UsernameNotFoundException {
         final Customer customer = repository
-                .findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User with email: '" + username + "' not found."));
+                .findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username: '" + username + "' not found."));
         return toUserDetails(customer);
     }
 
     private UserDetails toUserDetails(Customer customer) {
-        final String email = customer.getEmail();
+        final String username = customer.getUsername();
         final String password = customer.getPassword();
         final String role = customer.getRole();
         return User.builder()
-                .username(email)
+                .username(username)
                 .password(password)
                 .roles(role)
                 .build();
