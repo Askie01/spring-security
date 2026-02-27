50d7acb0d07bc7884a87bc991a2fbc2567cc0c21
Adam Pawlowski <adam.paw01@gmail.com>
2026-02-27 16:38:05 +0100

    Update column definitions to uppercase in `01-customer-schema.sql` file
    * Updated column definitions for `Customer` entity in `01-customer-schema.sql` file to uppercase letters - for convenience & cleaner look.

--- sql/schema/01-customer-schema.sql
+++ sql/schema/01-customer-schema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE customer
 (
     id            VARCHAR(36) PRIMARY KEY,
-    firstName     varchar(100) NOT NULL,
-    lastName      varchar(100) NOT NULL,
-    email         varchar(100) NOT NULL,
-    mobile_number varchar(20)  NOT NULL,
-    username      varchar(500) NOT NULL,
-    password      varchar(500) NOT NULL,
-    role          varchar(100) NOT NULL,
-    created_at    TIMESTAMP    NOT NULL
+    firstName     VARCHAR(100)        NOT NULL,
+    lastName      VARCHAR(100)        NOT NULL,
+    email         VARCHAR(100) UNIQUE NOT NULL,
+    mobile_number VARCHAR(20) UNIQUE  NOT NULL,
+    username      VARCHAR(500) UNIQUE NOT NULL,
+    password      VARCHAR(500)        NOT NULL,
+    role          VARCHAR(100)        NOT NULL,
+    created_at    TIMESTAMP           NOT NULL
 );
