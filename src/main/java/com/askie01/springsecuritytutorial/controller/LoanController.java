3ef2627e651c08f873f53859223e20fb5129eef9
Adam Pawlowski <adam.paw01@gmail.com>
2026-03-02 22:18:38 +0100

    Update `LoanController`
    * Updated mentioned controller to retrieve list of loans for a specific `accountNumber`

--- src/main/java/com/askie01/springsecuritytutorial/controller/LoanController.java
+++ src/main/java/com/askie01/springsecuritytutorial/controller/LoanController.java
@@ -1,18 +1,37 @@
 package com.askie01.springsecuritytutorial.controller;
 
-import org.springframework.http.HttpStatus;
+import com.askie01.springsecuritytutorial.dto.LoanResponseBody;
+import com.askie01.springsecuritytutorial.mapper.LoanToLoanResponseBodyMapper;
+import com.askie01.springsecuritytutorial.service.LoanService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping(path = "/loans")
+@RequiredArgsConstructor
 public class LoanController {
 
+    private final LoanService service;
+    private final LoanToLoanResponseBodyMapper loanMapper;
+
     @GetMapping
-    public ResponseEntity<String> getLoan() {
-        final String loan = "Loan Details";
-        return new ResponseEntity<>(loan, HttpStatus.OK);
+    public ResponseEntity<List<LoanResponseBody>> getLoan(@RequestParam Long accountNumber) {
+        final List<LoanResponseBody> responseBody = service
+                .getAccountLoans(accountNumber)
+                .stream()
+                .map(loanMapper::mapToDTO)
+                .toList();
+        return ResponseEntity
+                .ok()
+                .cacheControl(CacheControl.maxAge(60, TimeUnit.SECONDS))
+                .body(responseBody);
     }
 }
