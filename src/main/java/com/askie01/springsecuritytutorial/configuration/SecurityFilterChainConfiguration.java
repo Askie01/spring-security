5f7220ba204dd3b44a0460b4200b1d8f177cdb4d
Adam Pawlowski <adam.paw01@gmail.com>
2026-03-02 22:23:00 +0100

    Update `SecurityFilterChainConfiguration`
    * Updated endpoints security configuration - due to recent API changes.

--- src/main/java/com/askie01/springsecuritytutorial/configuration/SecurityFilterChainConfiguration.java
+++ src/main/java/com/askie01/springsecuritytutorial/configuration/SecurityFilterChainConfiguration.java
@@ -3,6 +3,7 @@ package com.askie01.springsecuritytutorial.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -25,16 +26,17 @@ public class SecurityFilterChainConfiguration {
                         .expiredUrl("/error/session-expired")
                 )
                 .authorizeHttpRequests(requestConfiguration -> requestConfiguration
-                        .requestMatchers("/accounts").authenticated()
-                        .requestMatchers("/cards").authenticated()
-                        .requestMatchers("/loans").authenticated()
-                        .requestMatchers("/balances").authenticated()
-                        .requestMatchers("/notices").permitAll()
-                        .requestMatchers("/contacts").permitAll()
-                        .requestMatchers("/error").permitAll()
-                        .requestMatchers("/customers").permitAll()
-                        .requestMatchers("/error/invalid-session").permitAll()
-                        .requestMatchers("/error/session-expired").permitAll()
+                        .requestMatchers("/accounts/**").authenticated()
+                        .requestMatchers("/cards/**").authenticated()
+                        .requestMatchers("/loans/**").authenticated()
+                        .requestMatchers("/transactions/**").authenticated()
+                        .requestMatchers("/notices/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/contacts/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/contacts/**").authenticated()
+                        .requestMatchers("/customers/**").permitAll()
+                        .requestMatchers("/error/**").permitAll()
+                        .requestMatchers("/error/invalid-session/**").permitAll()
+                        .requestMatchers("/error/session-expired/**").permitAll()
                 )
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(httpBasic -> httpBasic
