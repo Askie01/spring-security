29388030d1dd2e6e6b7a6ccbcb31b1093b7f45e8
Adam Pawlowski <adam.paw01@gmail.com>
2026-03-02 17:36:18 +0100

    Update `NoticeController`
    * Updated GET request in the mentioned controller to return list of notices in a DTO format, sorted by newest ones.

--- src/main/java/com/askie01/springsecuritytutorial/controller/NoticeController.java
+++ src/main/java/com/askie01/springsecuritytutorial/controller/NoticeController.java
@@ -1,18 +1,31 @@
 package com.askie01.springsecuritytutorial.controller;
 
-import org.springframework.http.HttpStatus;
+import com.askie01.springsecuritytutorial.dto.NoticeResponseBody;
+import com.askie01.springsecuritytutorial.mapper.NoticeToNoticeResponseBodyMapper;
+import com.askie01.springsecuritytutorial.service.NoticeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/notices")
+@RequiredArgsConstructor
 public class NoticeController {
 
+    private final NoticeService service;
+    private final NoticeToNoticeResponseBodyMapper noticeMapper;
+
     @GetMapping
-    public ResponseEntity<String> getNotice() {
-        final String notice = "Notice Details";
-        return new ResponseEntity<>(notice, HttpStatus.OK);
+    public ResponseEntity<List<NoticeResponseBody>> getNotices() {
+        final List<NoticeResponseBody> notices = service
+                .getNotices()
+                .stream()
+                .map(noticeMapper::mapToDTO)
+                .toList();
+        return ResponseEntity.ok(notices);
     }
 }
