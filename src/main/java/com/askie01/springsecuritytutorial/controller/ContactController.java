50439924b066775bf72f5aa444c9ca243c5e62cd
Adam Pawlowski <adam.paw01@gmail.com>
2026-03-02 21:58:58 +0100

    Update `ContactController`
    * Updated mentioned controller with an additional endpoints to create contacts and retrieve all contacts, as well as search by subject.

--- src/main/java/com/askie01/springsecuritytutorial/controller/ContactController.java
+++ src/main/java/com/askie01/springsecuritytutorial/controller/ContactController.java
@@ -1,18 +1,55 @@
 package com.askie01.springsecuritytutorial.controller;
 
-import org.springframework.http.HttpStatus;
+import com.askie01.springsecuritytutorial.dto.ContactRequestBody;
+import com.askie01.springsecuritytutorial.dto.ContactResponseBody;
+import com.askie01.springsecuritytutorial.mapper.ContactToContactResponseBodyMapper;
+import com.askie01.springsecuritytutorial.service.ContactService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping(path = "/contacts")
+@RequiredArgsConstructor
 public class ContactController {
 
+    private final ContactService service;
+    private final ContactToContactResponseBodyMapper contactMapper;
+
+    @PostMapping
+    public ResponseEntity<Void> createContact(@RequestBody ContactRequestBody requestBody) {
+        service.createContact(requestBody);
+        final String subject = requestBody.getSubject();
+        final URI uri = URI.create("/contacts?subject=" + subject);
+        return ResponseEntity
+                .created(uri)
+                .build();
+    }
+
     @GetMapping
-    public ResponseEntity<String> getContact() {
-        final String contact = "Contact details";
-        return new ResponseEntity<>(contact, HttpStatus.OK);
+    public ResponseEntity<List<ContactResponseBody>> getContact(@RequestParam String subject) {
+        final List<ContactResponseBody> responseBody = service
+                .getContacts(subject)
+                .stream()
+                .map(contactMapper::mapToDTO)
+                .toList();
+        return ResponseEntity.ok(responseBody);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ContactResponseBody>> getContact() {
+        final List<ContactResponseBody> responseBody = service
+                .getContacts()
+                .stream()
+                .map(contactMapper::mapToDTO)
+                .toList();
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(60, TimeUnit.SECONDS))
+                .body(responseBody);
     }
 }
