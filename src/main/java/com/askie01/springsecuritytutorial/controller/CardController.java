c24d676cf75e045a531c6d8194666dd3872d949e
Adam Pawlowski <adam.paw01@gmail.com>
2026-03-02 17:23:01 +0100

    Update `CardController`
    * Updated mentioned controller with improved GET endpoint - which takes `accountNumber` from a request path, and returns list of `CardResponseBody` objects with card details for a specific account.

--- src/main/java/com/askie01/springsecuritytutorial/controller/CardController.java
+++ src/main/java/com/askie01/springsecuritytutorial/controller/CardController.java
@@ -1,17 +1,32 @@
 package com.askie01.springsecuritytutorial.controller;
 
+import com.askie01.springsecuritytutorial.dto.CardResponseBody;
+import com.askie01.springsecuritytutorial.mapper.CardToCardResponseBodyMapper;
+import com.askie01.springsecuritytutorial.service.CardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/cards")
+@RequiredArgsConstructor
 public class CardController {
 
-    @GetMapping
-    public ResponseEntity<String> getCard() {
-        final String card = "Card Details";
-        return ResponseEntity.ok(card);
+    private final CardService service;
+    private final CardToCardResponseBodyMapper cardMapper;
+
+    @GetMapping("/{accountNumber}")
+    public ResponseEntity<List<CardResponseBody>> getAccountCards(@PathVariable Long accountNumber) {
+        final List<CardResponseBody> cards = service
+                .getAccountCards(accountNumber)
+                .stream()
+                .map(cardMapper::mapToDTO)
+                .toList();
+        return ResponseEntity.ok(cards);
     }
 }
