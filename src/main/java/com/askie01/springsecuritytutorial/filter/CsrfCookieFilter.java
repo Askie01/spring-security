364ddee2f52364f5dd5a6a72193c83267817a8f2
Adam Pawlowski <adam.paw01@gmail.com>
2026-03-09 12:31:01 +0100

    Create `CsrfCookieFilter`
    * Created filter to retrieve cookie CSRF cookie value.

---
+++ src/main/java/com/askie01/springsecuritytutorial/filter/CsrfCookieFilter.java
@@ -0,0 +1,21 @@
+package com.askie01.springsecuritytutorial.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.SneakyThrows;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class CsrfCookieFilter extends OncePerRequestFilter {
+
+    @Override
+    @SneakyThrows
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) {
+        final CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+        csrfToken.getToken();
+        filterChain.doFilter(request, response);
+    }
+}
