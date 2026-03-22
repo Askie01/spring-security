e321fc4b92781fbea083c185a1fb63b1d14e7e89
Adam Pawlowski <adam.paw01@gmail.com>
2026-03-22 11:47:44 +0100

    Add `02-customer_authority-data.sql` file. Update data scripts order. Fix column name in `01-customer-data.sql`
    * Added new data.sql file for customer authorities.
    * Updated the existing data script order for correct SQL scripts execution.
    * Fixed small issue with the column name to be snake case, instead of camel case in `01-customer-data.sql` script.

---
+++ src/main/resources/data/02-customer_authority-data.sql
@@ -0,0 +1,8 @@
+INSERT INTO customer_authority(customer_id, authority)
+VALUES ('4af9df8f-4dc6-4e13-9e3a-78456f487d9c', 'USER');
+
+INSERT INTO customer_authority(customer_id, authority)
+VALUES ('4af9df8f-4dc6-4e13-9e3a-78456f487d9c', 'ADMIN');
+
+INSERT INTO customer_authority(customer_id, authority)
+VALUES ('4af9df8f-4dc6-4e13-9e3a-78456f487d9c', 'DEVELOPER');
